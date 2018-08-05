9cbdc244cb17a9690bb0f1ab1cbe96ca5aa3a476
poppabear8883 <servnx@gmail.com>
2018-08-05 01:03:17 -0400

    fix: 18.04 Compatibility issue with pngquant-bin
    Error during `npm install` regarding `pngquant-bin`
    
    OR
    
    An error similar to `... binary doesn't seem to work correctly`

--- src/Installer/UNIT3D/Unit3dSetup.php
+++ src/Installer/UNIT3D/Unit3dSetup.php
@@ -111,6 +111,9 @@ class Unit3dSetup extends BaseInstaller
         $this->process([
             'supervisorctl reread',
             'supervisorctl update',
+            'wget -q -O /tmp/libpng12.deb http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb',
+            'dpkg -i /tmp/libpng12.deb',
+            'rm /tmp/libpng12.deb'
         ]);
 
         $www_cmds = [
