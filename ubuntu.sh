c38a730d3598dd6bb984c7193fb8861c523b9121
poppabear8883 <servnx@gmail.com>
2018-08-09 11:42:54 -0400

    feat: Add non-zero exit code checks for more gracefully error handling

--- ubuntu.sh
+++ ubuntu.sh
@@ -9,33 +9,72 @@ rm -rf /var/cache/debconf/*.*
 echo -e "\n\n$Purple Preparing Environment For The Installer ... $Color_Off"
 echo "============================================="
 
-if ! grep -q "^deb .*$ondrej/php" /etc/apt/sources.list /etc/apt/sources.list.d/* > /dev/null 2>&1; then
+# Adds PPA's
+add_ppa() {
     echo -e "\n$Cyan Adding PPA Repositories ... $Color_Off"
-    add-apt-repository -y ppa:nginx/development > /dev/null 2>&1
-    add-apt-repository -y ppa:ondrej/php > /dev/null 2>&1
-    add-apt-repository -y ppa:certbot/certbot > /dev/null 2>&1
-    echo -e "$IGreen OK $Color_Off"
-fi
 
-if ! [ -x "$(command -v php)" ]; then
+    for ppa in "$@"; do
+        add-apt-repository -y $ppa > /dev/null 2>&1
+        check $? "Adding $ppa Failed!"
+    done
+
+    echo -e "$IGreen OK $Color_Off"
+}
+
+# Installs Environment Prerequisites
+add_pkgs() {
+    # Update apt
     echo -e "\n$Cyan Updating Packages ... $Color_Off"
+
     apt-get -qq update
+    check $? "Updating packages Failed!"
+
     echo -e "$IGreen OK $Color_Off"
 
+    # PHP
     echo -e "\n$Cyan Installing PHP ... $Color_Off"
+
     apt-get install -qq curl debconf-utils php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml php7.2-fpm > /dev/null
-    echo -e "$IGreen OK $Color_Off"
-fi
+    check $? "Installing PHP Failed!"
 
-if ! [ -x "$(command -v composer)" ]; then
+    echo -e "$IGreen OK $Color_Off"
+}
+
+# Installs Composer
+install_composer() {
     echo -e "\n$Cyan Installing Composer ... $Color_Off"
-    php -r "readfile('http://getcomposer.org/installer');" | sudo php -- --install-dir=/usr/bin/ --filename=composer > /dev/null
-    echo -e "$IGreen OK $Color_Off"
-fi
 
-echo -e "\n$Cyan Adding Installer Packages ... $Color_Off"
-composer install > /dev/null 2>&1
-echo -e "$IGreen OK $Color_Off"
+    php -r "readfile('http://getcomposer.org/installer');" | sudo php -- --install-dir=/usr/bin/ --filename=composer > /dev/null
+    check $? "Installing Composer Failed!"
+
+    echo -e "$IGreen OK $Color_Off"
+}
+
+# Adds installer packages
+installer_pkgs() {
+    echo -e "\n$Cyan Adding Installer Packages ... $Color_Off"
+
+    composer install > /dev/null 2>&1
+    check $? "Adding Installer Packages Failed!"
+
+    echo -e "$IGreen OK $Color_Off"
+}
+
+# Checks the returned code
+check() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$Red Error: $2 \n Please try re-running the script via 'sudo ./install.sh' $Color_Off"
+        exit $1
+    fi
+}
+
+add_ppa ppa:nginx/development ppa:ondrej/php ppa:certbot/certbot
+
+add_pkgs
+
+install_composer
+
+installer_pkgs
 
 echo -e "\n$Purple Launching The Installer ... $Color_Off"
 echo "============================================="
