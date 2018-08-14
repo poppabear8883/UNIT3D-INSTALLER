b9a0452ec5da8331ca973f1e4c7e62a9bd4a4779
poppabear8883 <servnx@gmail.com>
2018-08-14 12:10:09 -0400

    fix: Add supervisor reload
    Ref: https://github.com/HDInnovations/UNIT3D/issues/371

--- src/Installer/UNIT3D/Unit3dSetup.php
+++ src/Installer/UNIT3D/Unit3dSetup.php
@@ -119,6 +119,7 @@ class Unit3dSetup extends BaseInstaller
         $this->process([
             'supervisorctl reread',
             'supervisorctl update',
+            'supervisorctl reload',
             'wget -q -O /tmp/libpng12.deb http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb',
             'dpkg -i /tmp/libpng12.deb',
             'rm /tmp/libpng12.deb'
